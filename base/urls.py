9e1350b67b33178b69f4450d04478933f1a80201
chetas61 <cnshinde1@gmail.com>
2022-05-11 20:26:55 +0530

    Profile page added

--- base/urls.py
+++ base/urls.py
@@ -8,6 +8,7 @@ urlpatterns =[
 
     path('',views.home, name="home"),
     path('room/<str:pk>/', views.room,  name="room"),
+    path('profile/<str:pk>/', views.userProfile, name='user-profile'),
     path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
 
     path('create-room/', views.createRoom, name="create-room"),
